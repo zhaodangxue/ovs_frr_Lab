2d3ab8218f9c0479027ffe514c112e1758c93ffb
zhaodangxue <z404583637@sjtu.edu.cn>
2024-07-17 12:16:39 +0800

    add  test_all

--- README.md
+++ README.md
@@ -20,7 +20,13 @@ apt install openvswitch-switch
 * **create_ovs_br.sh**:创建对应的ovs网桥，并且进行验证。
 * **ovs_br0_config.sh**:为对应的br0作相关配置，即用ovs-docker指令来增加相关port。
 * **delete_ovs_br.sh**:删除对应port后再删除网桥，并且进行验证。
-* **set_advertise_X目录**:这个目录下的脚本会为对应的容器进行对应的frr配置，唯一变量是advertisement Interval，这里的变量就是我们的X
-* **set_host目录**:这个目录负责创建四个host节点挂载到4个leaf节点上，也负责对应的节点删除
+* **set_advertise_X目录**:这个目录下的脚本会为对应的容器进行对应的frr配置，唯一变量是advertisement Interval，这里的变量就是我们的X。
+* **set_host目录**:这个目录负责创建四个host节点挂载到4个leaf节点上，也负责对应的节点删除。
 * **setup.py**:这个负责安装对应的网络拓扑，包括spine，leaf，host节点和对应网桥的设置。
 * **teardown.py**:这个负责销毁之前setup设置的环境，销毁创建的br0网桥和所有节点相关的容器。
+* **reconnect_test.py**:重联收敛测试，根据不同的advertise interval来看重联收敛的变化。
+* **tcp_test.py**:tcp打流测试，输入**-b**链路带宽参数来对不同链路带宽进行测试。
+* **udp_test.py**:udp打流测试，输入**-B**链路带宽参数**-b**的iperf发包速率来进行对应打流测试。
+* **tcp_send_self.py**:tcp不限制链路带宽发包给h1本身和本身的deviceIP，测试最大性能。
+* **udp_send_self.py**:udp发包给h1本身和本身的deviceIP，限制iperf的发包速率，测试性能。
+* **test_all.py**:以上多个测试的聚合形态，打包了reconnect_test，tcp_test，udp_test，tcp_send_self，udp_send_self这些测试。
---
+++ script/test_all.py
@@ -0,0 +1,58 @@
+import os
+import argparse
+import subprocess
+import time
+import termcolor as T
+def log(s, col="green"):
+    print (T.colored(s, col))
+def main():
+    python3 = "python3 "
+    os.mkdir("/home/ovs_data")
+    log("Testing Reconnect Test---")
+    # advertise time = 5
+    os.system(python3+"setup.py -a 5")
+    time.sleep(5)
+    os.system(python3+"reconnect_test.py")
+    os.system(python3+"teardown.py")
+    # advertise time = 10
+    os.system(python3+"setup.py -a 10")
+    time.sleep(5)
+    os.system(python3+"reconnect_test.py")
+    os.system(python3+"teardown.py")
+    # advertise time = 15
+    os.system(python3+"setup.py -a 15")
+    time.sleep(5)
+    os.system(python3+"reconnect_test.py")
+    os.system(python3+"teardown.py")
+    log("Complete Reconnect Test!")
+    os.system(python3+"setup.py -a 5")
+    time.sleep(5)
+    log("Testing TCP Arguement Test---")
+    os.system(python3+"tcp_test.py -b 1000")
+    os.system(python3+"tcp_test.py -b 800")
+    os.system(python3+"tcp_test.py -b 600")
+    os.system(python3+"tcp_test.py -b 400")
+    os.system(python3+"tcp_test.py -b 200")
+    os.system(python3+"tcp_test.py -b 100")
+    log("Complete TCP Arguement Test!")
+    log("Testing Send Self Test---")
+    os.system(python3+"tcp_send_self.py")
+    os.system(python3+"udp_send_self.py")
+    log("Complete Send Self Test!")
+    log("Testing UDP Arguement Test---")
+    os.system(python3+"udp_test.py -B 1000 -b 1000")
+    os.system(python3+"udp_test.py -B 1000 -b 600")
+    os.system(python3+"udp_test.py -B 1000 -b 200")
+    os.system(python3+"udp_test.py -B 600 -b 1000")
+    os.system(python3+"udp_test.py -B 600 -b 600")
+    os.system(python3+"udp_test.py -B 600 -b 200")
+    os.system(python3+"udp_test.py -B 200 -b 1000")
+    os.system(python3+"udp_test.py -B 200 -b 600")
+    os.system(python3+"udp_test.py -B 200 -b 200")
+    log("Complete UDP Arguement Test!")
+    log("Teardown---")
+    os.system(python3+"teardown.py")
+    log("Copy Data---")
+    os.system("cp -r ./data /home/ovs_data")
+if __name__ == "__main__":
+    main()
